73d25e2f18566b6b949395d03cd381221cc52dc9
chaychen <chaychen@zhangruideMacBook-Air.local>
2021-01-29 11:13:38 +0800

    update case

--- app/src/main/java/org/fisco/bcos/sdk/demo/MainActivity.java
+++ app/src/main/java/org/fisco/bcos/sdk/demo/MainActivity.java
@@ -3,18 +3,16 @@ package org.fisco.bcos.sdk.demo;
 import android.os.Bundle;
 import android.os.Environment;
 import androidx.appcompat.app.AppCompatActivity;
-import java.math.BigInteger;
-import java.util.List;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.fisco.bcos.sdk.BcosSDKForProxy;
 import org.fisco.bcos.sdk.NetworkHandler.NetworkHandlerImp;
-import org.fisco.bcos.sdk.abi.datatypes.generated.tuples.generated.Tuple2;
 import org.fisco.bcos.sdk.client.Client;
+import org.fisco.bcos.sdk.client.exceptions.NetworkHandlerException;
 import org.fisco.bcos.sdk.client.protocol.response.BcosTransaction;
 import org.fisco.bcos.sdk.client.protocol.response.BcosTransactionReceipt;
 import org.fisco.bcos.sdk.config.model.ProxyConfig;
-import org.fisco.bcos.sdk.demo.contract.HelloWorldProxy;
+import org.fisco.bcos.sdk.demo.contract.HelloWorld;
 import org.fisco.bcos.sdk.log.BcosSDKLogUtil;
 import org.fisco.bcos.sdk.model.CryptoType;
 import org.fisco.bcos.sdk.model.NodeVersion;
@@ -55,38 +53,35 @@ public class MainActivity extends AppCompatActivity {
                                     Client client = sdk.getClient(1);
                                     NodeVersion nodeVersion = client.getClientNodeVersion();
                                     logger.info("node version: " + JsonUtils.toJson(nodeVersion));
-                                    HelloWorldProxy sol =
-                                            HelloWorldProxy.deploy(
+                                    HelloWorld sol =
+                                            HelloWorld.deploy(
                                                     client,
-                                                    client.getCryptoSuite().getCryptoKeyPair(),
-                                                    "Hello world.");
+                                                    client.getCryptoSuite().getCryptoKeyPair());
                                     logger.info(
                                             "deploy contract , contract address: "
                                                     + JsonUtils.toJson(sol.getContractAddress()));
                                     // HelloWorldProxy sol =
-                                    // HelloWorldProxy.load("0x2ffa020155c6c7e388c5e5c9ec7e6d403ec2c2d6", client, client.getCryptoSuite().getCryptoKeyPair());
+                                    // HelloWorldProxy.load("0x2ffa020155c6c7e388
+                                    // c5e5c9ec7e6d403ec2c2d6", client,
+                                    // client.getCryptoSuite().getCryptoKeyPair());
                                     TransactionReceipt ret1 = sol.set("Hello, FISCO BCOS.");
                                     logger.info("send, receipt: " + JsonUtils.toJson(ret1));
                                     String ret2 = sol.get();
                                     logger.info("call to return string, result: " + ret2);
-                                    List<String> ret3 = sol.getList();
-                                    logger.info(
-                                            "call to return list, result: "
-                                                    + JsonUtils.toJson(ret3));
-                                    Tuple2<String, BigInteger> ret4 = sol.getTuple();
-                                    logger.info(
-                                            "call to return tuple, result: "
-                                                    + JsonUtils.toJson(ret4));
-                                    BcosTransaction ret5 =
+                                    BcosTransaction transaction =
                                             client.getTransactionByHash(ret1.getTransactionHash());
                                     logger.info(
                                             "getTransactionByHash, result: "
-                                                    + JsonUtils.toJson(ret5));
-                                    BcosTransactionReceipt ret6 =
+                                                    + JsonUtils.toJson(transaction.getResult()));
+                                    BcosTransactionReceipt receipt =
                                             client.getTransactionReceipt(ret1.getTransactionHash());
                                     logger.info(
                                             "getTransactionReceipt, result: "
-                                                    + JsonUtils.toJson(ret6));
+                                                    + JsonUtils.toJson(receipt.getResult()));
+                                } catch (NetworkHandlerException e) {
+                                    logger.error(
+                                            "NetworkHandlerException error info: "
+                                                    + e.getMessage());
                                 } catch (Exception e) {
                                     logger.error("error info: " + e.getMessage());
                                     e.printStackTrace();
